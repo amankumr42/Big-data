22fee93a99770c2058b016fb4f8e444ce114ba4a
root <root@localhost.localdomain>
2020-04-13 09:22:42 -0400

    Started wih PySpark

---
+++ PySpark/PySparkCode/Started_With_PySpark.py
@@ -0,0 +1,13 @@
+# load data into spark as data frame
+flighData2015 = spark.read.option("inferschema","true").option("header","true").csv(../Data/flight-data/csv/2015-summary.csv)
+
+# count number of records in data frame
+flighData2015.count()
+
+# Fetch the number of records from data frame
+flighData2015.take(3)
+
+# Explain the partition logic
+flighData2015.sort("count").explain()
+
+
